b26cecffc16eaf6b40cdab4a83b58757afb5c3eb
Soupy <stuart.dilts@gmail.com>
2015-04-20 22:11:09 -0600

    renamed file BinarySearchTree

---
+++ binarySearchTree/AVLTree.java
@@ -0,0 +1,82 @@
+package binarySearchTree;
+
+/**
+ * A balanced AVL binary tree implementation
+ * @author sdilts
+ */
+public class AVLTree<T extends Comparable> extends AbstractBalanceableTree<T> {
+    //get new createNode method for lower functions:
+    @Override
+    protected BalNode<T> createNode(T e) {
+        return new BalNode<T>(e);
+    }
+
+    public AVLTree() {
+	super();
+    }
+    
+    protected int getHeight(Node n) {
+	if(n != null) {
+	    if(n instanceof BalNode) {
+		return ((BalNode)n).getAux();
+	    } else throw new IllegalArgumentException("Node given is not of type 'BalNode'");
+	} else return 0;
+    }
+
+    protected void computeHeight(Node n) {
+	((BalNode)n).setAux(1+ Math.max(getHeight(n.getLeft()), getHeight(n.getRight())));
+    }
+
+    protected boolean isBallanced(Node n) {
+	return Math.abs(getHeight(n.getLeft()) - getHeight(n.getRight())) <= 1;
+    }
+
+    protected Node tallerChild(Node n) {
+	if(getHeight(n.getLeft()) > getHeight(n.getRight())) {
+	    return n.getLeft();
+	} else if(getHeight(n.getLeft()) < getHeight(n.getRight())) {
+	    return n.getRight();
+	} else if(n.equals(root)) {
+	    return n.getRight();
+	} else if(n.equals(n.getLeft().getParent())){
+	    return n.getLeft();
+	} else return n.getRight();
+    }
+
+    public void balance(Node current) {
+	int oldHeight,newHeight;
+	do {
+	    oldHeight = getHeight(current);
+	    if(!isBallanced(current)) {
+		current = restructure(tallerChild(tallerChild(current)));
+		computeHeight(current.getLeft());
+		computeHeight(current.getRight());
+	    }
+	    computeHeight(current);
+	    newHeight = getHeight(current);
+	    current = current.getParent();
+	} while(newHeight != oldHeight && current != null);
+
+    }
+
+    @Override
+    public void insert(T val) {
+	//create node to be inserted
+	Node newNode = createNode(val);
+	super.insertNode(newNode);
+	balance(newNode);
+    }
+
+    @Override
+    public boolean remove(T val) {
+	Node remove = lookupNode(root, val);
+	if(super.remove(remove)) {
+	    if(!remove.equals(root)) {
+		//System.out.println("remoing");
+		balance(remove.getParent());
+	    }
+	    return true;
+	} else return false;
+    }
+}
+
---
+++ binarySearchTree/AbstractBalanceableTree.java
@@ -0,0 +1,71 @@
+package binarySearchTree;
+
+public abstract class AbstractBalanceableTree<T extends Comparable> extends BinarySearchTree<T> {
+        //class for extending our node functionality:
+    protected static class BalNode<E extends Comparable> extends Node<E> {
+    	//extra data needed for ballancing:
+    	int aux = 0;
+
+    	public BalNode(E val) {
+    	    super(val);
+    	}
+	
+    	public void setAux(int val) {
+    	    aux = val;
+    	}
+
+    	public int getAux() {
+    	    return aux;
+    	}
+    }
+    //----------END OF BalNode CLASS---------
+    
+    protected abstract BalNode<T> createNode(T e);
+
+    public void rotateNode(T val) {
+	rotate(getNode(val));
+    }
+
+    protected void rotate(Node pivot) {
+	Node parent = pivot.getParent();
+	Node grandpa = parent.getParent(); //may be null
+
+	if(grandpa == null) {
+	    root = pivot;
+	    pivot.setParent(null);
+	} else {
+	    if(grandpa.getLeft().equals(parent)) {
+		grandpa.setLeft(pivot);
+		pivot.setParent(grandpa);
+	    } else {
+		grandpa.setRight(pivot);
+		pivot.setParent(grandpa);
+	    }
+	}
+	if(parent.getLeft().equals(pivot)) {
+	    Node s = pivot.getRight();
+	    pivot.setRight(parent);
+	    parent.setParent(pivot);
+	    parent.setLeft(s);
+	} else {
+	    Node s = pivot.getLeft();
+	    pivot.setLeft(parent);
+	    parent.setRight(s);
+	    parent.setParent(pivot);
+	}
+    }
+
+    public Node restructure(Node pivot) {
+	//check nodes are in straigth line:
+	//if both statments are true or if both statements are false:
+	if((pivot.equals(pivot.getParent().getRight()))                               //if pivot is to the right of parent
+	   == (pivot.getParent().equals(pivot.getParent().getParent().getRight()))) { //if parent is to the right of grandpa
+	    rotate(pivot.getParent());
+	    return pivot.getParent();
+	} else { //Nodes are not in straight line:
+	    rotate(pivot);
+	    rotate(pivot);
+	    return pivot;
+	}
+    }
+}
